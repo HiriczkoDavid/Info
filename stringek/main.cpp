7aad94c70d9bd60bc19f0414838b114b7d933eb5
szegedibettina <47552022+szegedibettina@users.noreply.github.com>
2019-10-08 09:10:07 +0300

    Add files via upload

---
+++ stringek/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string a;
+  string b("Ez egy string") ;
+  cout << "a=" ;
+  cin >>a;
+  cout << a << " " << a.length() << endl;
+  cout << b << " " << b.size() << endl;
+
+
+
+
+
+
+    return 0;
+}
