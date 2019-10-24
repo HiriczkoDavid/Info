ccf1f3e267dfd0bd6e474271178406e6e9a66ebe
szegedibettina <47552022+szegedibettina@users.noreply.github.com>
2019-10-24 07:14:31 +0300

    Add files via upload

---
+++ Ketdimenzios tombok(2. feladat)/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+
+  int N,M,i,j;
+  cout << "N=";
+  cin >> N;
+  int v[N];
+  int matrix [N][M] ;
+  for (i=0; i<N; i++) {
+    v[i]=rand()%100;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
