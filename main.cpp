fe1b409633a41b13f96b0fe8d701f03c3f0b12ac
szegedibettina <47552022+szegedibettina@users.noreply.github.com>
2020-02-20 10:36:41 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,50 +1,35 @@
 #include <iostream>
 #include <fstream>
-
 using namespace std;
 
 int main()
 {
+     int N,i,j;
+    ifstream in ("input.txt");
+    ofstream out("output.txt");
+    in>>N;
+    int v[100][100];
+    int w[100][100];
+    for(i=0;i<N;i++){
+        for (j=0;j<N;j++){
+            in>>v[i][j];
 
-   int N,i,j,a,b;
-   int falatti = 0,ffolotti = 0;
-
-   ifstream in ("input.txt") ;
-   ofstream out ("lkkt.out") ;
-
-   in >> N;
-
-   int matrix[N][N] ;
-
-   for (i=0; i<N; i++) {
-    for (j=0; j<N; j++) {
-            in >> matrix [i][j] ;
-    if (i<j) {
-        ffolotti=ffolotti+matrix[i][j];
-    }
-    if (i>j) {
-        falatti=falatti+matrix[i][j] ;
-    }
-    }
-   }
-    a=ffolotti;
-    b=falatti;
-    while (a!=b) {
-        if (a>b) {
-            a=a-b;
-        }
-        if (a<b) {
-            b=b-a;
         }
     }
-
-   out << ffolotti*falatti/a;
-
-
-
-
-
-
-
+    for(i=0;i<N;i++){
+        for (j=0;j<N;j++){
+            if(   v[i][j]>v[i][j+1] && (v[i][j]>v[i-1][j+1] || (i-1 < 0))&& v[i][j]>v[i+1][j+1] && (v[i][j]>v[i-1][j] || (i-1 < 0)) && v[i][j]>v[i+1][j]  && (v[i][j]>v[i-1][j-1] || ((i-1 < 0) || (j-1 < 0))) && (v[i][j]>v[i][j-1] || (j-1 < 0))  && (v[i][j]>v[i+1][j-1] || (i-1 < 0)))
+                {
+                    w[i][j]=1;
+                }else {
+                w[i][j]=0;
+                }
+        }
+    }
+    for(i=0;i<N;i++){
+        for (j=0;j<N;j++){
+            out<<w[i][j]<<" ";
+        }out<<endl;
+    }
     return 0;
 }
